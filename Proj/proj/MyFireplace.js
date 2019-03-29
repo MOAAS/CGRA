c259d46d21b40f2d6e27d2c0651cc75aca43f254
Daniel <38920805+VextoN@users.noreply.github.com>
2019-03-29 20:59:47 +0000

    firethingy mini update

--- Proj/proj/MyFireplace.js
+++ Proj/proj/MyFireplace.js
@@ -7,19 +7,35 @@ class MyFireplace extends ObjectGroup {
         this.fireWood1 = new MyCilinder(scene, 8);
         this.fireWood2 = new MyCilinder(scene, 8);
         this.fireWood3 = new MyCilinder(scene, 8);
-
+        this.fireWood4 = new MyCilinder(scene, 8);
+        this.fireWood5 = new MyCilinder(scene, 8);
         this.fireWoods = new ObjectGroup(scene);
-        this.fireWoods.addObjects(this.fireWood1, this.fireWood2,this.fireWood3);
+        this.fireWoods.addObjects(this.fireWood1, this.fireWood2,this.fireWood3,this.fireWood4,this.fireWood5);
+        this.fireWoods.scale(0.25,2,0.25);
+        this.fireWoods.rotate(Math.PI/8,1,0,0);
+        this.fireWood2.rotate(Math.PI/5,0,1,0);
+        this.fireWood3.rotate(2*Math.PI/5,0,1,0);
+        this.fireWood4.rotate(3*Math.PI/5,0,1,0);
+        this.fireWood5.rotate(4*Math.PI/5,0,1,0);
+        this.fireWood1.translate(0,0,-1);
+        this.fireWood2.translate(-0.951,0,-0.309);
+        this.fireWood3.translate(-0.588,0,0.809);
+        this.fireWood4.translate(0.588,0,0.809);
+        this.fireWood5.translate(0.951,0,-0.309);
 
-        // ----- Fire -------
-        this.fire= new MyCone(scene, 8);
 
 
         this.fireWoodMaterial = new MyCGFappearance(scene, 0.3, 0.8, 0.4, 10);
-        this.fireMaterial = new MyCGFappearance(scene, 0.2, 0.8, 0.8, 25);
+        this.fireWoods.setMaterial(this.fireWoodMaterial);
+        this.fireWoods.setTexture(firewoodTexture);
 
-        this.fireWoods.setMaterial(this.fireWoodMaterial)
-        this.fire.setMaterial(this.fireMaterial)
+        // ----- Fire -------
+        this.fire= new MyCone(scene, 8);
+        this.fire.translate(0,50,0);
+        this.fireMaterial = new MyCGFappearance(scene, 0.2, 0.8, 0.8, 25);
+        this.fire.setMaterial(this.fireMaterial);
+        this.fire.setTexture(fireTexture);
+
 
         this.addObjects(this.fire, this.fireWoods);
 
