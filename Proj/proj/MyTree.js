6fed41535f3ff889c3daa0b8319d48334b67ecc4
MOAAS <38762849+MOAAS@users.noreply.github.com>
2019-04-02 15:16:36 +0100

    cooler trees

--- Proj/proj/MyTree.js
+++ Proj/proj/MyTree.js
@@ -24,17 +24,17 @@ class MyTreeGroupPatch extends ObjectGroup {
     constructor(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture) {
         super(scene);
 
-        var minDistBetweenTrees = Math.max(trunkRadius * 2, treeTopRadius * 2) * 1.25;
+        var minDistBetweenTrees = Math.max(trunkRadius * 2, treeTopRadius * 2) * 1.5;
 
-        this.tree1 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree2 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree3 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree4 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree5 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree6 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree7 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree8 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree9 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
+        this.tree1 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree2 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree3 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree4 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree5 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree6 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree7 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree8 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree9 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
 
         this.addObjects(this.tree1,this.tree2, this.tree3, this.tree4, this.tree5, this.tree6, this.tree7, this.tree8, this.tree9)
 
@@ -51,19 +51,19 @@ class MyTreeRowPatch extends ObjectGroup {
 
         var minDistBetweenTrees = Math.max(trunkRadius * 2, treeTopRadius * 2) * 1.25;
 
-        this.tree1 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree2 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree3 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree4 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree5 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
-        this.tree6 = new MyTree(scene, trunkHeight, trunkRadius, treeTopHeight, treeTopRadius, trunkTexture, treeTopTexture);
+        this.tree1 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree2 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree3 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree4 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree5 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
+        this.tree6 = new MyTree(scene, trunkHeight * getRandNum(0.9, 1.2), trunkRadius * getRandNum(0.9, 1.1), treeTopHeight * getRandNum(1, 1.5), treeTopRadius * getRandNum(0.9, 1.1), trunkTexture, treeTopTexture);
 
         this.addObjects(this.tree1,this.tree2, this.tree3, this.tree4, this.tree5, this.tree6)
 
         var patchLength = 0;
         for (var i = 0; i < this.objects.length; i++) {
-            var xDisp = minDistBetweenTrees * getRandNum(1, 1.25);
-            var zDisp = minDistBetweenTrees * getRandNum(-0.3, 0.3);
+            var xDisp = minDistBetweenTrees * getRandNum(1, 1.2);
+            var zDisp = minDistBetweenTrees * getRandNum(-0.2, 0.2);
             this.objects[i].translate(patchLength + xDisp, 0, zDisp);
             patchLength += xDisp;
         }
